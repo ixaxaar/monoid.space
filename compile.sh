62a92767f3483aad62ac7c055a48762722e30eeb
ixaxaar <root@ixaxaar.in>
2019-02-13 22:25:59 +0530

    remove junk

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 declare -a files=(
   "intro"
   "contents"
@@ -12,10 +11,15 @@ declare -a files=(
   "Lang/proofsAsData"
 )
 
+rm -rf build html tmp
+mkdir html
 
 for i in "${files[@]}"
 do
    echo "Compiling" "${i}.ladga.md"
-   agda --compile --compile-dir=./build "${i}.lagda.md"
+   agda --compile --no-main --html --html-dir=./tmp --css=./css --compile-dir=./build "${i}.lagda.md"
+   echo "Generating HTML for " "${i/\//\.}.html"
+   pandoc -o ./html/"${i/\//\.}.html" ./tmp/"${i/\//\.}.html"
 done
 
+rm -rf tmp
