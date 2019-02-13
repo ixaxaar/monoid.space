ca1aa0552ea17f1e61f8ebbe337779f01211ed50
ixaxaar <root@ixaxaar.in>
2019-02-14 00:47:59 +0530

    add files

--- compile.sh
+++ compile.sh
@@ -17,9 +17,15 @@ mkdir html
 for i in "${files[@]}"
 do
    echo "Compiling" "${i}.ladga.md"
+   doctoc "${i}.lagda.md"
+   echo """
+   [Contents](./contents.html)
+   """ >> "${i}.ladga.md"
+   sed -i "s/\*generated with \[DocToc\](https:\/\/github.com\/thlorenz\/doctoc)\*//g" "${i}.lagda.md"
    agda --compile --no-main --html --html-dir=./tmp --css=./css --compile-dir=./build "${i}.lagda.md"
-   echo "Generating HTML for " "${i/\//\.}.html"
-   pandoc -o ./html/"${i/\//\.}.html" ./tmp/"${i/\//\.}.html"
+   echo "Generating HTML for " "${i}.lagda.md"
+   pandoc -s -S --css=../css/agda.css --from=markdown_github --to=html --columns=80 -o ./html/"${i/\//\.}.html" "${i}.lagda.md"
+   git checkout "${i}.ladga.md"
 done
 
 rm -rf tmp
